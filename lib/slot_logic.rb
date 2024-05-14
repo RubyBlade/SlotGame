abed1bad75d717d29e7a4351a0d320e55ec47682
RubyBlade <aliakseikrukouski@gmail.com>
2024-05-14 19:49:42 +0200

    Added loop cycle to the game spins and test updates

--- lib/slot_logic.rb
+++ lib/slot_logic.rb
@@ -4,6 +4,7 @@ class SlotGame
   end
 
   def spin
+   loop do
     screen = generate_screen
     display_screen(screen)
 
@@ -12,6 +13,11 @@ class SlotGame
     else
       puts "No win this time. Try again!"
     end
+
+    puts "Press Enter to spin again or type 'exit' to quit."
+    input = gets.chomp.downcase
+    break if input == 'exit'
+   end
   end
 
   private
